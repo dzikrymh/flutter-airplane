56292d4381a2e7f28fd8dbef5a1606786164b317
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2022-03-23 11:48:41 +0700

    add cubit for auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/checkout_page.dart';
 import 'package:airplane/ui/pages/choose_seat_page.dart';
@@ -7,28 +8,42 @@ import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:airplane/ui/pages/success_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => SplashPage(),
-        '/get-started': (context) => GetStartedPage(),
-        '/sign-up': (context) => SignUpPage(),
-        '/bonus': (context) => BonusPage(),
-        '/main': (context) => MainPage(),
-        '/detail': (context) => DetailPage(),
-        '/choose-seat': (context) => ChooseSeatPage(),
-        '/checkout': (context) => CheckoutPage(),
-        '/success-checkout': (context) => SuccessCheckoutPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PageCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => SplashPage(),
+          '/get-started': (context) => GetStartedPage(),
+          '/sign-up': (context) => SignUpPage(),
+          '/bonus': (context) => BonusPage(),
+          '/main': (context) => MainPage(),
+          '/detail': (context) => DetailPage(),
+          '/choose-seat': (context) => ChooseSeatPage(),
+          '/checkout': (context) => CheckoutPage(),
+          '/success-checkout': (context) => SuccessCheckoutPage(),
+        },
+      ),
     );
   }
 }
